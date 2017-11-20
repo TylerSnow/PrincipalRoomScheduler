029004570791d258d1a7fa36b32bf9de6393417c
John <jph432@mun.ca>
2017-11-20 12:33:57 -0330

    Add panels for each area

--- HomePanel.java
+++ HomePanel.java
@@ -18,35 +18,34 @@ public class HomePanel extends JPanel {
 	public HomePanel() {
 		setLayout(new BorderLayout(0, 0));
 		
+		//Welcome
+		JPanel welcomePanel = new JPanel();
+		welcomePanel.setPreferredSize(new Dimension(20, 50));
 		JLabel welcomeLabel = new JLabel("Scheduletron 3000");
 		welcomeLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		add(welcomeLabel, BorderLayout.NORTH);
-		welcomeLabel.setPreferredSize(new Dimension(20, 50));
 		
+		//Description
 		JTextArea description = new JTextArea();
 		description.setText("Welcome to the school scheduling program.\nTo proceed, press one of the buttons below.");
 		add(description, BorderLayout.CENTER);
 		
-		//#######################################################################################################
+		//Buttons
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setPreferredSize(new Dimension(20, 50));
-        buttonPanel.setMaximumSize(new Dimension(100, 600));
 		add(buttonPanel, BorderLayout.SOUTH);
 		buttonPanel.setLayout(new GridLayout(0, 2));
 		buttonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
 		JButton principalButton = new JButton("Enter as Principal");
-		//principalButton.setPreferredSize(new Dimension(10, 30));
 		int margin = 10;
 		principalButton.setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
 		JButton applicantButton = new JButton("Enter as Applicant");
-		//applicantButton.setPreferredSize(new Dimension(10, 30));
 		applicantButton.setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
-
 		
 		buttonPanel.add(principalButton);
 		buttonPanel.add(applicantButton);
-		//#######################################################################################################
+		
 
 	}
 
