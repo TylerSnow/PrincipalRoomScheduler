95bd1ebc85e88d7919786a34de63e7876bde8281
John <jph432@mun.ca>
2017-11-24 10:42:25 -0330

    Add initial principal panels

---
+++ PrincipalLogin.java
@@ -0,0 +1,66 @@
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JPasswordField;
+
+public class PrincipalLogin extends JPanel {
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Create the panel.
+	 */
+	public PrincipalLogin() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblWelcome = new JLabel("Welcome");
+		GridBagConstraints gbc_lblWelcome = new GridBagConstraints();
+		gbc_lblWelcome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblWelcome.gridx = 1;
+		gbc_lblWelcome.gridy = 1;
+		add(lblWelcome, gbc_lblWelcome);
+		
+		JLabel lblUsername = new JLabel(" Username:");
+		GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+		gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsername.anchor = GridBagConstraints.EAST;
+		gbc_lblUsername.gridx = 3;
+		gbc_lblUsername.gridy = 5;
+		add(lblUsername, gbc_lblUsername);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 4;
+		gbc_textField.gridy = 5;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPassword.anchor = GridBagConstraints.EAST;
+		gbc_lblPassword.gridx = 3;
+		gbc_lblPassword.gridy = 6;
+		add(lblPassword, gbc_lblPassword);
+		
+		passwordField = new JPasswordField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 0, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 4;
+		gbc_passwordField.gridy = 6;
+		add(passwordField, gbc_passwordField);
+
+	}
+
+}
