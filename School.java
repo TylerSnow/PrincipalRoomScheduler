dcc480e964fef0fb91d748701040768aebd3fdec
Josh <joshclarke453@gmail.com>
2017-11-25 19:49:04 -0330

    Made School and Account classes, turned everything to Lists instead of Arrays

---
+++ School.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class School {
+	private List<Room> roomList;
+	private String id;
+	private List<Request> pendingRequests;
+	private List<Account> authorizedAccounts;
+	private List<Group> groupList;
+	
+	public School(List<Room> nRoomList, String nId, List<Request> nPendingRequests, List<Account> nAuthorizedAccounts, List<Group> nGroupList) {
+		roomList = nRoomList;
+		id = nId;
+		pendingRequests = nPendingRequests;
+		authorizedAccounts = nAuthorizedAccounts;
+		groupList = nGroupList;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public List<Room> getRoomList(){
+		return roomList;
+	}
+	
+	public List<Request> getPendingRequests(){
+		return pendingRequests;
+	}
+	
+	public List<Account> getAuthorizedAccounts(){
+		return authorizedAccounts;
+	}
+	
+	public List<Group> getGroupList(){
+		return groupList;
+	}
+	
+	public void addRoom(Room newRoom) {
+		roomList.add(newRoom);
+	}
+}
