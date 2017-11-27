2b1086ebf20b195d65a7d0cf7a48e5a77d958740
joshclarke453 <32573814+joshclarke453@users.noreply.github.com>
2017-11-27 10:29:51 -0330

    Created "numSlots" in Request

--- school/Request.java
+++ school/Request.java
@@ -3,14 +3,16 @@ import java.time.LocalDate;
 
 public class Request {
 	private Applicant applicant;
+	private int numSlots;
 	private Room room;
 	private Booking booking;
 	private int priority;
 	private String message;
 	private LocalDate dateRequested;
 	
-	public Request(Applicant nApplicant, Room r, Booking b, int nPriority, String nMessage, LocalDate nDateRequested) {
+	public Request(Applicant nApplicant, int nNumSlots, Room r, Booking b, int nPriority, String nMessage, LocalDate nDateRequested) {
 		applicant = nApplicant;
+		numSlots = nNumSlots;
 		room = r;
 		booking = b;
 		priority = nPriority;
@@ -20,6 +22,10 @@ public class Request {
 	
 	public Applicant getApplicant() {
 		return applicant;
+	}
+	
+	public int getNumSlots() {
+		return numSlots;
 	}
 	
 	public Room getRoom() {
