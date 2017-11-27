58c42e28caa318ebed8eeec0475ce58184e36835
Josh <joshclarke453@gmail.com>
2017-11-27 09:46:29 -0330

    Changed to static, made sample requestss

--- school/School.java
+++ school/School.java
@@ -8,7 +8,7 @@ public class School {
 	private List<Room> roomList;
 	private String id;
 	private static List<Request> pendingRequests;
-	private List<Account> authorizedAccounts;
+	private static List<Account> authorizedAccounts;
 	private List<Group> groupList;
 	
 	public School(List<Room> nRoomList, String nId, List<Request> nPendingRequests, List<Account> nAuthorizedAccounts, List<Group> nGroupList) {
@@ -18,6 +18,7 @@ public class School {
 		authorizedAccounts = nAuthorizedAccounts;
 		groupList = nGroupList;
 		authorizedAccounts.add(makeAdmin());
+		createSampleRequests();
 	}
 	
 	public String getId() {
@@ -32,7 +33,7 @@ public class School {
 		return pendingRequests;
 	}
 	
-	public List<Account> getAuthorizedAccounts(){
+	public static List<Account> getAuthorizedAccounts(){
 		return authorizedAccounts;
 	}
 	
@@ -49,12 +50,18 @@ public class School {
 		return admin;
 	}
 	
-	/*
 	public static void createSampleRequests() {
-		Group g = new Group("bys");
-		pendingRequests.add(new Request(new Applicant("hey", g), new Room("Gymnasium", 2),
-				new Booking(LocalTime.parse("02:00"), LocalTime.parse("02:00"), Semester.FALL, DayOfWeek.MONDAY, g), 
-				1, "balls", LocalDate.now()));
+		Group MathClub = new Group("Math Club");
+		pendingRequests.add(new Request(new Applicant("Steve", MathClub), new Room("Gymnasium", 2),
+				new Booking(LocalTime.parse("02:00"), LocalTime.parse("02:00"), Semester.FALL, DayOfWeek.MONDAY, MathClub), 
+				1, "Math Competition", LocalDate.now()));
+		Group HockeyTeam = new Group("Hockey Team");
+		pendingRequests.add(new Request(new Applicant("Bob", HockeyTeam), new Room("ChemLab", 1),
+				new Booking(LocalTime.parse("03:00"), LocalTime.parse("03:00"), Semester.FALL, DayOfWeek.TUESDAY, HockeyTeam),
+				2, "Practice", LocalDate.now()));
+		Group ChessClub = new Group("Chess Club");
+		pendingRequests.add(new Request(new Applicant("Jim", ChessClub), new Room("Library", 3),
+				new Booking(LocalTime.parse("04:00"), LocalTime.parse("04:00"), Semester.FALL, DayOfWeek.THURSDAY, ChessClub),
+				3, "Tournament", LocalDate.now()));
 	}
-	*/
 }
