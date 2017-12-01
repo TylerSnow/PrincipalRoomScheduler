d295536671522f72a88a75dbca146cf4c5dec7f8
John <jph432@mun.ca>
2017-12-01 12:39:42 -0330

    Fix writerequest bug

--- school/WriteRequest.java
+++ school/WriteRequest.java
@@ -13,25 +13,6 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class WriteRequest {
-
-	public static void main(String args[]) {
-
-		WriteRequest obj = new WriteRequest();
-		Group skatingTeam = new Group("Tastes bad");
-
-		Booking[] sb1 = {
-				new Booking(LocalTime.parse("01:00"),LocalTime.parse("02:00"),Semester.WINTER, DayOfWeek.TUESDAY, skatingTeam),
-				new Booking(LocalTime.parse("04:00"),LocalTime.parse("05:00"),Semester.WINTER, DayOfWeek.THURSDAY, skatingTeam),
-				new Booking(LocalTime.parse("12:00"),LocalTime.parse("13:00"),Semester.WINTER, DayOfWeek.SUNDAY, skatingTeam),
-				new Booking(LocalTime.parse("08:00"),LocalTime.parse("09:00"),Semester.WINTER, DayOfWeek.SATURDAY, skatingTeam),
-				new Booking(LocalTime.parse("05:00"),LocalTime.parse("09:00"),Semester.WINTER, DayOfWeek.MONDAY, skatingTeam),
-				};
-		Request req = new Request(new Applicant("Joe", skatingTeam), 3, Semester.WINTER, new Room("Auditorium", 4), sb1, "Skating Praccy");
-
-
-		obj.serializeAddress(req);
-
-	}
 	
 	public static void removeRequest(int n) {
 		String filePath = new File("").getAbsolutePath();
@@ -65,9 +46,15 @@ public class WriteRequest {
 			//fout = new FileOutputStream("C:\\Users\\John.DESKTOP-DT12DV9\\Documents\\address.ser");
 			String filePath = new File("").getAbsolutePath();
 			filePath += "\\src\\objects\\requests";
-			int numRequests = new File(filePath).list().length;
-			String fileName = "\\request" + numRequests;
-			fout = new FileOutputStream(filePath + fileName);
+			if (new File(filePath).list() != null) {
+				int numRequests = new File(filePath).list().length;
+				String fileName = "\\request" + numRequests;
+				fout = new FileOutputStream(filePath + fileName);
+			}
+			else {
+				String fileName = "\\request0";
+				fout = new FileOutputStream(filePath + fileName);
+			}
 			oos = new ObjectOutputStream(fout);
 			oos.writeObject(r);
 
